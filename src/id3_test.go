c0d716e1833227896ef023b0ceb7bd77a0ce0ce6
Andrew Scherkus <ascherkus@gmail.com>
2011-08-21 16:38:12 -0700

    Checking in basic test files for v2.2, v2.3 and v2.4 cases.

--- src/id3_test.go
+++ src/id3_test.go
@@ -16,12 +16,101 @@ package id3
 
 import (
 	"bytes"
+	"path"
+	"os"
 	"testing"
 )
+
+type fileTest struct {
+	path string
+	file File
+}
+
+var fileTests = []fileTest{
+	fileTest{"test_220.mp3", File{ID3v2Header{2, 0, false, false, false, false, 226741},
+		"There There", "Radiohead", "Hail To The Thief", "2003", "9", "", "(20)", ""}},
+	fileTest{"test_230.mp3", File{ID3v2Header{3, 0, false, false, false, false, 150717},
+		"Everything In Its Right Place", "Radiohead", "Kid A", "2000", "1", "", "Alternative", ""}},
+	fileTest{"test_240.mp3", File{ID3v2Header{4, 0, false, false, false, false, 165126},
+		"Give Up The Ghost", "Radiohead", "The King Of Limbs", "2011", "07/08", "1/1", "Alternative", ""}},
+}
 
 func TestEmpty(t *testing.T) {
 	file := Read(new(bytes.Buffer))
 	if file != nil {
 		t.Fail()
+	}
+}
+
+func TestFiles(t *testing.T) {
+	for _, expected := range fileTests {
+		p := path.Join("test", expected.path)
+		t.Log(p)
+
+		fd, err := os.Open(p)
+		if fd == nil || err != nil {
+			t.Error(err)
+			continue
+		}
+		defer fd.Close()
+
+		actual := Read(fd)
+		if actual == nil {
+			t.Error("Could not parse ID3 information")
+			continue
+		}
+
+		// ID3v2Header fields.
+		header := expected.file.Header
+		if actual.Header.Version != header.Version {
+			t.Errorf("Header.Version: expected %d got %d", header.Version, actual.Header.Version)
+		}
+		if actual.Header.MinorVersion != header.MinorVersion {
+			t.Errorf("Header.MinorVersion: expected %d got %d", header.MinorVersion, actual.Header.MinorVersion)
+		}
+		if actual.Header.Unsynchronization != header.Unsynchronization {
+			t.Errorf("Header.Unsynchronization: expected %t got %t", header.Unsynchronization, actual.Header.Unsynchronization)
+		}
+		if actual.Header.Extended != header.Extended {
+			t.Errorf("Header.Extended: expected %t got %t", header.Extended, actual.Header.Extended)
+		}
+		if actual.Header.Experimental != header.Experimental {
+			t.Errorf("Header.Experimental: expected %t got %t", header.Experimental, actual.Header.Experimental)
+		}
+		if actual.Header.Footer != header.Footer {
+			t.Errorf("Header.Footer: expected %t got %t", header.Footer, actual.Header.Footer)
+		}
+		if actual.Header.Size != header.Size {
+			t.Errorf("Header.Size: expected %d got %d", header.Size, actual.Header.Size)
+		}
+
+		// Name, Artist, etc...
+		file := expected.file
+		if actual.Name != file.Name {
+			t.Errorf("Name: expected %s got %s", file.Name, actual.Name)
+		}
+		if actual.Artist != file.Artist {
+			t.Errorf("Artist: expected %s got %s", file.Artist, actual.Artist)
+		}
+		if actual.Album != file.Album {
+			t.Errorf("Album: expected %s got %s", file.Album, actual.Album)
+		}
+		if actual.Year != file.Year {
+			t.Errorf("Year: expected %s got %s", file.Year, actual.Year)
+		}
+		if actual.Track != file.Track {
+			t.Errorf("Track: expected %s got %s", file.Track, actual.Track)
+		}
+		if actual.Disc != file.Disc {
+			t.Errorf("Disc: expected %s got %s", file.Disc, actual.Disc)
+		}
+		if actual.Genre != file.Genre {
+			t.Errorf("Genre: expected %s got %s", file.Genre, actual.Genre)
+		}
+		if actual.Length != file.Length {
+			t.Errorf("Length: expected %s got %s", file.Length, actual.Length)
+		}
+
+		t.Log()
 	}
 }
